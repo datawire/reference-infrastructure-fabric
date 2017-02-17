93829b7d0e79e285b62ba4cd7a7c2fc56fb158ab
Philip Lombardi <plombardi@datawire.io>
2017-02-17 11:32:45 -0500

    Remove shared stuff from the fabric/example branch as it doesn't belong there anymore.

--- modules/vpc/outputs.tf
+++
@@ -1,6 +0,0 @@
-// file: modules/vpc/outputs.tf
-
-output "id"                 { value = "${aws_vpc.main.id}" }
-output "cidr_block"         { value = "${aws_vpc.main.cidr_block}" }
-output "external_subets"    { value = "${aws_subnet.external.*}" }
-output "availability_zones" { value = "${var.availability_zones}" }
